d95213802f2efbcf2019972d246da6a347de3f44
sriharitirumala <sritirumala30@gmail.com>
2023-02-28 11:40:49 +0530

    [Token-1] Adding test pods

--- 01-Sample/test ifle.tf
+++ 01-Sample/test ifle.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3-micro"
-  vpc_security_group_ids = [sg-0f914773417860a28]
+  vpc_security_group_ids = [vpc-0559526174e73c9c6]
   tags = {
     Name = "test pod"
   }
