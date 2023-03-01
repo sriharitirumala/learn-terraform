25eaa603a656ff2a37479ae543c1790441bf878a
sriharitirumala <sritirumala30@gmail.com>
2023-03-01 16:02:43 +0530

    [Token-1] Adding frontend pod

--- 04-s3-state/main.tf
+++ 04-s3-state/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-b71-services-site"
+    key = "04-s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
