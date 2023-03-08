f6a775c495a975ada2ec8f7edfc43670d003a3f1
sriharitirumala <sritirumala30@gmail.com>
2023-03-08 18:05:39 +0530

    [Story-1] adding correct Repos

---
+++ 09-functions/main.tf
@@ -0,0 +1,14 @@
+variable "test" {
+  default = {
+    "catalogue" = {
+      "private_ip" = "172.31.5.212"
+    }
+    "user" = {
+      "private_ip" = "172.31.12.137"
+    }
+  }
+}
+
+output "test" {
+  value = var.test["catalogue"].private_ip
+}
