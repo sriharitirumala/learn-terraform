8538d20a245f662b4e165edac77655bbd77cb61d
sriharitirumala <sritirumala30@gmail.com>
2023-03-09 10:31:41 +0530

    [Story-1] adding correct Repos

---
+++ roboshop-setup/ex5/route53/main.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "record" {
+  zone_id = "Z099374713TEKOU3HRH9T"
+  name    = "${var.component}-dev.devopsb71services.site"
+  type    = "A"
+  ttl     = 30
+  records = [var.private_ip]
+}
+
+variable "private_ip" {}
+variable "component" {}
+
