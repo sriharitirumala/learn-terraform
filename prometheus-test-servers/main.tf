06b6f16e332ecdaec652f23d7c2778e15bee728f
sriharitirumala <sritirumala30@gmail.com>
2023-03-15 13:50:50 +0530

    [Story-1] adding correct Repos

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -9,6 +9,12 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami      = "ami-0194f9cb4cd268dc5"
   instance_type = "t3.micro"
@@ -17,4 +23,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-node"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
