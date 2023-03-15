6fbf72db828d17c5c09239cc9b96905c29ccc3ab
sriharitirumala <sritirumala30@gmail.com>
2023-03-15 11:28:30 +0530

    [Story-1] adding correct Repos

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "node1" {
   ami      = "ami-0194f9cb4cd268dc5"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0f914773417860a28]
+  vpc_security_group_ids = ["sg-0f914773417860a28"]
   wait_for_fulfillment = true
 
   tags = {
@@ -12,7 +12,7 @@ resource "aws_spot_instance_request" "node1" {
 resource "aws_spot_instance_request" "node2" {
   ami      = "ami-0194f9cb4cd268dc5"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0f914773417860a28]
+  vpc_security_group_ids = ["sg-0f914773417860a28"]
 
   tags = {
     Name= "prom-test-server"
