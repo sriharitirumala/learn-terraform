9a5fd3e46ae86bd300cfc9a86539c7a64544f074
sriharitirumala <sritirumala30@gmail.com>
2023-04-24 12:45:34 +0530

    Ammend by SrihariHUB@gmail.com

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -19,6 +19,7 @@ resource "aws_spot_instance_request" "node2" {
   ami      = "ami-0194f9cb4cd268dc5"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0f914773417860a28"]
+  wait_for_fulfillment = true
 
   tags = {
     Name = "prom-test-node"
