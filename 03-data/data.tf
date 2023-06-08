6e7b6ed64b958a0c95ae5dd7b5c84cbe929fae45
sriharitirumala <sritirumala30@gmail.com>
2023-06-08 10:41:08 +0530

    aws

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "example" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["119044227534"]
+  owners      = ["278783648585"]
 }
   output "ami_id" {
     value=data.aws_ami.image_id
--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami"  {
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
-  owners = ["119044227534"]
+  owners = ["278783648585"]
 }
 
 
